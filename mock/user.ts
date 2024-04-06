68b0d5f46ca09bb18c0426790275ad1eddc99d3b
zxx <ss15037500392@163.com>
2024-04-06 10:24:37 +0800

    登录

--- mock/user.ts
+++ mock/user.ts
@@ -39,7 +39,7 @@ export default [
       if (!checkUser) {
         return { code: 202, data: { message: '获取用户信息失败' } }
       }
-      return { code: 200, data: checkUser}
+      return { code: 200, data: checkUser }
     },
   },
 
